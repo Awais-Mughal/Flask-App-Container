64c6da330b5096054ab0b2cb9b976ed575a28fe3
Awais-Mughal <awaismughal0045@gmail.com>
2021-05-10 17:37:55 +0500

    Edit 2

--- app/app.py
+++ app/app.py
@@ -11,10 +11,24 @@ app = Flask(__name__)
 
 def home():
     Hname = socket.gethostname()
-    return Hname
+    return ('Hostname is: ' + Hname)
 
 @app.route("/index1/")
-def ind():
+def ind1():
+    Hname = socket.gethostname()
+    image = 'pic/r.jpg'
+    return render_template('index.html', hostname = Hname, pic = image )
+
+
+@app.route("/index2/")
+def ind2():
+    Hname = socket.gethostname()
+    image = 'pic/s.jpg'
+    return render_template('index.html', hostname = Hname, pic = image )
+
+
+@app.route("/index3/")
+def ind3():
     return redirect("https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80")
                 
 
